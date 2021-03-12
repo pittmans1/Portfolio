cb7f8f12fdad29dcb7e49c5457d9908e426b409e
scott pittman <sp.digital97@gmail.com>
2021-03-11 22:46:03 -0500

    finished first portfolio project functionality and css working

--- my-personal-site/src/Home.js
+++ my-personal-site/src/Home.js
@@ -3,6 +3,7 @@ import React, { useState } from "react"
 function Home(props){
     const dataLove = Array.from(props.loveAdvice)
     const dataLife = Array.from(props.lifeAdvice)
+    
     const [imgToggle, setImgToggle] = useState(true)
     const [imgToggleTwo, setImgToggleTwo] = useState(true)
     const [imgToggleThree, setImgToggleThree]= useState(true)
@@ -10,19 +11,23 @@ function Home(props){
     const lover = love[Math.floor(Math.random() * love.length)]
     const life = dataLife.map(advice => advice.advice)
     const lifer = life[Math.floor(Math.random() * life.length)]
+ 
     return(
         <div id="home">
             <div id="welcome">
-            <h2>Welcome</h2>
-            <div>
-                <p>
-                    welcome to the advice page please click on the picture with the coresponding keyword for the advice you need to hear!
-                </p>
+                <div>
+                     <h2>Welcome</h2>
+                </div>
+                 <div>
+                      <p>
+                       welcome to the advice page please click on the picture with the coresponding keyword for the advice you need to hear!
+                     </p>
+                </div>
             </div>
                 <div id="imgOne" onClick={()=> setImgToggle(prev => !prev)}>
                     {imgToggle?
                     <>
-                    <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuwothrknZ_T4kNmJ7zcC5o6hoTemc62g7cw&usqp=CAU" style={{"pointerEvents": "all"}}/>
+                    <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuwothrknZ_T4kNmJ7zcC5o6hoTemc62g7cw&usqp=CAU" style={{"pointerEvents": "all"}} style={{"width":"80%"}}/>
                     <p>Love</p>
                     </>
                        : 
@@ -33,7 +38,7 @@ function Home(props){
                 <div id="imgTwo" onClick = {() => setImgToggleTwo(prev => !prev)}>
                     {imgToggleTwo?
                     <>
-                    <img src="https://knowledge.insead.edu/sites/www.insead.edu/files/images/2020/04/gettyimages-628537464-1.jpg"/>
+                    <img src="https://knowledge.insead.edu/sites/www.insead.edu/files/images/2020/04/gettyimages-628537464-1.jpg" style = {{"width":"75%"}}/>
                     <p>Life</p>
                     </>
                     : 
@@ -43,7 +48,7 @@ function Home(props){
                 <div id= "imgThree" onClick = {() => setImgToggleThree(prev => !prev)}>
                         {imgToggleThree?
                         <>
-                        <img src="https://i.pinimg.com/originals/57/ff/cc/57ffcc4f2b055a8f815c51a7bebd79c5.jpg"/>
+                        <img src="https://i.pinimg.com/originals/57/ff/cc/57ffcc4f2b055a8f815c51a7bebd79c5.jpg" style={{"width":"75%"}}/>
                         <p>Random Advice!!</p>
                         </>
                         :
@@ -51,8 +56,8 @@ function Home(props){
 
 
                 </div>
-            </div>
-       </div>
+         </div>
+     
     )
 }
 
---
+++ my-personal-site/src/advice/Why.js
@@ -0,0 +1,18 @@
+import React from "react"
+function Why() {
+    return(
+        <div>
+            <h1 style={{"textAlign":"center"}}>Why?</h1>
+            <hr></hr>
+            <p>The question everyone asks.
+                Well here is the reason why advice is important.
+                We dont have the answer to everything.
+                Some wish we did but others believe it is essential for humans to be able to ask for help or for advice it helps us grow and helps us see things in a new light.
+                I believe everyone needs a little advice, you never know when it could truly come in handy or when it may apply to something you dont realise.
+                Me personally i love asking for advice or another individuals input, i have gotten some amazing advice over the years. I hope this application gives you some good advice you never know it may apply tomorrow!
+            </p>
+            <img src="https://www.artofliving.org/sites/www.artofliving.org/files/wysiwyg_imageupload/18Oct2016%282%29%281%29.jpg" style={{"justifyContent":"center"}}/>
+        </div>
+    )
+}
+export default Why
